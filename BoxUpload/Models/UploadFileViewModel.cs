0fe379763992c6e54da09ebaf6368200509ad856
prat <prat.s@iconext.co.th>
2019-05-13 10:57:48 +0700

    initial

---
+++ BoxUpload/Models/UploadFileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxUpload.Models
+{
+    public class UploadFileViewModel
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string DeveloperToken { get; set; }
+        public Uri uri { get; set; }
+    }
+}
