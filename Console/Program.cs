be791acaebfdb7db6ac96009b838ddc1c4cc5098
prat <prat.s@iconext.co.th>
2019-05-14 14:56:55 +0700

    Console App

---
+++ Console/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ApplicationCore.Helper;
+using Infrastructure.Repository;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Console
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration;
+        static void Main(string[] args)
+        {
+            // Db Context
+            var connectionString = "Server=localhost;Database=Box;user id=sa;password=P@ssw0rd";
+            var option = new DbContextOptionBuilder(connectionString);
+
+            FileUploadRepository fileUploadRepository = new FileUploadRepository(option);
+            var xx = fileUploadRepository.Gets();
+
+
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadLine();
+        }
+    }
+}
